f6aefa2e211d5a7bb05e02dc0b2d735ccf2a86cb
gokull-1998 <gokulakrishnanm1998@gmail.com>
2024-07-09 14:49:26 +0530

    bug fixed

--- react_learnings/help.md
+++ react_learnings/help.md
@@ -1,2 +1,3 @@
  # npm create vite@latest react_learnings
  # npm i
+ # npm run dev to run the app
--- react_learnings/src/App.jsx
+++ react_learnings/src/App.jsx
@@ -1,24 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import About from './pages/About'
-import Home from './pages/Home'
-import { Route, Routes } from 'react-router-dom';
-
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import About from './pages/About';
+import Home from './pages/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
- <>
- app
- <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
- </>
-  )
+    <Router>
+      <>
+        app
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </>
+    </Router>
+  );
 }
 
-export default App
+export default App;
